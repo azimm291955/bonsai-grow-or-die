ac3687de17a10d33bb1540810d90388bdc95d5df
Aaron Zimmerman <aaron.zimmerman@bonsaicultivation.com>
2026-03-31 21:59:30 -0600

    Add age verification gate on first visit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import BonsaiGame from "@/components/game/BonsaiGame";
+import AgeVerification from "@/components/AgeVerification";
 
 export default function Home() {
-  return <BonsaiGame />;
+  return (
+    <AgeVerification>
+      <BonsaiGame />
+    </AgeVerification>
+  );
 }
